70fdf68faedfdd923d8cecba62d00a739ee8a1f4
Waqas Zahick <waqaszahick@hotmail.com>
2021-03-25 02:15:44 +1100

    Fixed code and comitted all the files.

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("../startercode/assets/data/data.csv").then(function(scatData) {
+d3.csv("assets/data/data.csv").then(function(scatData) {
 
     // Step 1: Parse Data/Cast as numbers
     // ==============================
